576dfd05a7b009376f3fd0eb6171f5370a61d32e
ferdous725890 <ferdous725890@gmail.com>
2024-12-26 19:49:47 +0600

    Car-Details-Section

---
+++ src/Components/CardTable.jsx/SecondBanner.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Authcontext } from "../../shared Component/Authprovider/Authprovider";
+import axios from "axios";
+
+import LatestCar from "./LatestCar";
+
+const SecondBanner = () => {
+  const [latestCar, setLatestCar] = useState([]);
+  const { user } = useContext(Authcontext);
+  console.log(latestCar);
+
+  useEffect(() => {
+    fetchAlldata();
+  }, []);
+
+  const fetchAlldata = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_API_URL}/added_car/available_car/latest`)
+      .then((res) => setLatestCar(res.data));
+  };
+
+  return (
+    <div>
+      <h2>
+        Latest Data <span>{latestCar?.length}</span>{" "}
+
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-y-10">
+            {
+                latestCar.map(car => <LatestCar key={car._id} car={car}></LatestCar>)
+            }
+        </div>
+
+      
+      </h2>
+
+
+
+    </div>
+  );
+};
+
+export default SecondBanner;
